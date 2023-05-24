8b1f3dc0eabc43a6c0cafe0d6ea32f760d256e00
iliass-de <iliasshlm@gmail.com>
2023-05-24 22:48:08 +0200

    feat: display launch details on modal

--- client/src/components/Launch.vue
+++ client/src/components/Launch.vue
@@ -1,22 +1,41 @@
+
 <template>
+
+  <Modal 
+     v-model:visible="data.modelIsVisible.value"
+      title="Details"
+      type="clean"
+  >
+    <h2>Details</h2>
+    <b> {{ data.singleLaunch.value.mission_name }}</b>
+    <div> 
+          {{ 
+              data.singleLaunch.value.details != null 
+              ? data.singleLaunch.value.details 
+              : "(no details are available)"  
+          }}
+    </div>
+  </Modal>
+  <div> Click an item to view details.</div>
   <ag-grid-vue
     class="ag-theme-alpine"
     style="height: 500px"
     :columnDefs="columnDefs.value"
-    :rowData="rowData.value"
+    :rowData="data.rowData.value"
     :defaultColDef="defaultColDef"
     rowSelection="multiple"
     animateRows="true"
-    @cell-clicked="cellWasClicked"
+    @cell-clicked="clickedCell"
     @grid-ready="onGridReady"
   >
   </ag-grid-vue>
+
  </template>
  
  <script>
  import { AgGridVue } from "ag-grid-vue3";  
- import { reactive, onMounted, ref } from "vue";
- 
+ import { reactive, onMounted, ref, unref } from "vue";
+ import { Modal } from 'usemodal-vue3';
  import "ag-grid-community/styles/ag-grid.css"; 
  import "ag-grid-community/styles/ag-theme-alpine.css"; 
  
@@ -24,22 +43,26 @@
   name: "App",
   components: {
     AgGridVue,
+    Modal
   },
   setup() {
     const gridApi = ref(null); 
+    const data = {
+      launchId : ref(null),
+      singleLaunch: ref(null),
+      rowData : ref(null),
+      modelIsVisible: ref(false)
+    };
  
     const onGridReady = (params) => {
       gridApi.value = params.api;
     };
- 
-    const rowData = reactive({}); 
- 
- 
+  
     const columnDefs = reactive({
       value: [
            { headerName: "Name", field: "name" },
            { headerName: "Fleight number", field: "flight_number" },
-           { headerName: "Details", field: "details" }
+           { headerName: "Date", field: "date_utc" }
       ],
     });
  
@@ -54,20 +77,41 @@
     onMounted(() => {
       fetch("https://localhost:7226/launchApi/launches")
         .then((result) => result.json())
-        .then((remoteRowData) => (rowData.value = remoteRowData));
+        .then((remoteRowData) => (data.rowData.value = remoteRowData));
     });
+
+    async function getSingleLaunch() {
+      const launchId = unref(data.launchId);
+      console.log(launchId);
+      await fetch(`https://localhost:7226/launchApi/launch/${launchId}`)
+      .then((result) => result.json())
+      .then((response) => (data.singleLaunch.value = response));
+       data.modelIsVisible.value = true;
+    }
  
     return {
       onGridReady,
+      data,
       columnDefs,
-      rowData,
       defaultColDef,
-      cellWasClicked: (event) => { 
-        console.log(event);
-      }
+      clickedCell: (event) => { 
+        data.launchId.value = event.data.flight_number;
+        getSingleLaunch();
+      },
+      getSingleLaunch
     };
   },
  };
  </script>
  
- <style lang="scss"></style>
+ <style>
+.modal-vue3-body{
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 4px;
+}
+.ag-header-cell-text{
+  margin-left: 10em;
+}
+
+</style>
